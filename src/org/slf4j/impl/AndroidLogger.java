1b4e84066bfa868c025b594f0f5d543f128cf0c6
rkemp <rkemp@ddd8df7e-4ee2-4ec5-83a0-ccc76e966a3c>
2009-09-28 14:13:32 +0000

    NOSLF4J.java support to turn off SL4J overhead for release code
    git-svn-id: https://gforge.cs.vu.nl/svn/interdroid/trunk/slf4android@521 ddd8df7e-4ee2-4ec5-83a0-ccc76e966a3c

--- src/org/slf4j/impl/AndroidLogger.java
+++ src/org/slf4j/impl/AndroidLogger.java
@@ -12,67 +12,70 @@ import org.slf4j.helpers.MessageFormatter;
 import android.util.Log;
 
 /**
- * <p>This class adapts the SLF4J interface to the Android Log system.
- * The intention is to make it easy to control logging from legacy
- * code systems properly. As such each log method checks to see if
- * that log level is enabled since the legacy code this adapts for
- * is often written with out such checks counting on the logging
- * system to handle filtering out these messages. This is not the
- * best thing for performance but we view it as the lesser of
- * two evils.
+ * <p>
+ * This class adapts the SLF4J interface to the Android Log system. The
+ * intention is to make it easy to control logging from legacy code systems
+ * properly. As such each log method checks to see if that log level is enabled
+ * since the legacy code this adapts for is often written with out such checks
+ * counting on the logging system to handle filtering out these messages. This
+ * is not the best thing for performance but we view it as the lesser of two
+ * evils.
  * </p>
- * <p>Note that most legacy logging system use a class name as the
- * tag. Android however requires tags be of a specific (and short)
- * length. As such all class names are automatically shortened in
- * some way. To see how the class name you are interested in gets
- * shortened monitor the 'slf4j' tag. Generally we try
- * to use the whole class name if it will fit but more often
- * this will get shortened to the first letters of each portion
- * of the path followed by the class name or simply the class
- * name if that will not fit.
+ * <p>
+ * Note that most legacy logging system use a class name as the tag. Android
+ * however requires tags be of a specific (and short) length. As such all class
+ * names are automatically shortened in some way. To see how the class name you
+ * are interested in gets shortened monitor the 'slf4j' tag. Generally we try to
+ * use the whole class name if it will fit but more often this will get
+ * shortened to the first letters of each portion of the path followed by the
+ * class name or simply the class name if that will not fit.
  * </p>
- * <p>Note also that we check isDebugEnabled with every call
- * despite this adding possible overhead since a great deal of
- * legacy code does not check the logging level first.
+ * <p>
+ * Note also that we check isDebugEnabled with every call despite this adding
+ * possible overhead since a great deal of legacy code does not check the
+ * logging level first.
  * </p>
- * <p>To configure this logger you can include an SLF4J.properties
- * file in your apk with lines of the form:<pre>
+ * <p>
+ * To configure this logger you can include an SLF4J.properties file in your apk
+ * with lines of the form:
+ * 
+ * <pre>
  * &lt;package&gt;.&lt;class&gt;=&lt;level&gt;
  * </pre>
- * where level is one of:
- * 'disabled', 'trace', 'debug', 'info', 'warn' or 'error'<br/>
- * You may also include a 'default.log.level=<level>' line
- * to set the default level for all classes. Note that
- * it is allowed to specify a class twice in which case the
- * lowest level specified in the file will be used except
- * for the default where the last instance in the file will be used.
+ * 
+ * where level is one of: 'disabled', 'trace', 'debug', 'info', 'warn' or
+ * 'error'<br/>
+ * You may also include a 'default.log.level=<level>' line to set the default
+ * level for all classes. Note that it is allowed to specify a class twice in
+ * which case the lowest level specified in the file will be used except for the
+ * default where the last instance in the file will be used.
  * </p>
- * <p>It is also possible to ask this logger to check the android
- * log level by adding "android.util.Log.check=true" to your
- * properties file.
+ * <p>
+ * It is also possible to ask this logger to check the android log level by
+ * adding "android.util.Log.check=true" to your properties file.
  * </p>
- * <p>We search for this properties file in the root of your JAR,
- * in the META-INF directory then the org/slf4j directory then
- * org/slf4j/impl directory and stop searching as soon as we
- * find one.
+ * <p>
+ * We search for this properties file in the root of your JAR, in the META-INF
+ * directory then the org/slf4j directory then org/slf4j/impl directory and stop
+ * searching as soon as we find one.
  * </p>
  **/
 public class AndroidLogger extends MarkerIgnoringBase {
 
-    /**
-     * Serial Version ID
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 * Serial Version ID
+	 */
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * Log tag for SLF4J itself
-     **/
-    private static final String SLF4J_TAG = "slf4j";
+	/**
+	 * Log tag for SLF4J itself
+	 **/
+	private static final String SLF4J_TAG = "slf4j";
 
-    /**
-     * The tag this logger will log with
-     */
-    private final String tag;
+	/**
+	 * The tag this logger will log with
+	 */
+	private final String tag;
 
 	private final int level;
 
@@ -86,9 +89,8 @@ public class AndroidLogger extends MarkerIgnoringBase {
 
 	private static int sDefaultLevel = DEFAULT_LOG_LEVEL;
 
-	private static final String[] levels = {
-		"disabled", "trace", "debug", "info", "warn", "error"
-	};
+	private static final String[] levels = { "disabled", "trace", "debug",
+			"info", "warn", "error" };
 	private static final String sConfigFile = "SLF4J.properties";
 
 	// This is dumb. You can't have an array of generics?
@@ -110,76 +112,93 @@ public class AndroidLogger extends MarkerIgnoringBase {
 	}
 
 	/**
-	   Android offers no way to turn off info and higher level messages
-	   This is not so great for integrating legacy services that
-	   expect a more advanced logger that can do some filtering.
-	   We thus use a property file in the form class.path.Class=level
-	   where level is one of "disabled", "trace", "debug", "info", "warn" or
-	   "error";
-	**/
+	 * Android offers no way to turn off info and higher level messages This is
+	 * not so great for integrating legacy services that expect a more advanced
+	 * logger that can do some filtering. We thus use a property file in the
+	 * form class.path.Class=level where level is one of "disabled", "trace",
+	 * "debug", "info", "warn" or "error";
+	 * 
+	 * If you're going to release your application you can include an empty
+	 * NOSLF4J.java in your source. This will turn off logging and increase
+	 * performance.
+	 **/
 	static {
-		// Load and parse the properties.
-		Log.d(SLF4J_TAG, "Trying to load properties from: " + sConfigFile);
-		InputStream in = AndroidLogger.class.getClassLoader().getResourceAsStream(sConfigFile);
-		if (in == null) {
-			Log.d(SLF4J_TAG, "Trying to load from: " + "META-INF/" + sConfigFile);
-			in = AndroidLogger.class.getClassLoader().getResourceAsStream("META-INF/" + sConfigFile);
-		}
-		if (in == null) {
-			Log.d(SLF4J_TAG, "Trying to load from: " + "org/slf4j/" + sConfigFile);
-			in = AndroidLogger.class.getClassLoader().getResourceAsStream("org/slf4j/" + sConfigFile);
-		}
-		if (in == null) {
-			Log.d(SLF4J_TAG, "Trying to load from: " + "org/slf4j/impl/" + sConfigFile);
-			in = AndroidLogger.class.getClassLoader().getResourceAsStream("org/slf4j/impl/" + sConfigFile);
-		}
-		if (null != in) {
-			Log.d(SLF4J_TAG, "Loading properties...");
-			Properties props = new java.util.Properties();
-			try {
-				props.load(in);
-				for (Enumeration<?> names = props.propertyNames(); names
-						.hasMoreElements();) {
-					String name = (String)names.nextElement();
-					if (name.equals(ANDROID_LEVEL_CHECK)) {
-						ignoreAndroidLevel = !Boolean.parseBoolean(props.getProperty(name));
-					}
-					// What level is this?
-					int value = parseLevel(props.getProperty(name));
-					if (value >= 0) {
-						if (DEFAULT_LEVEL_NAME.equals(name)) {
-							Log.d(SLF4J_TAG, "Setting default level to: " + levels[value]);
-							sDefaultLevel = value;
-						} else {
-							// Add it to the list for that level.
-							Log.d(SLF4J_TAG, "Setting level for: "
-									+ name + " to: " + levels[value]);
-							if (levelLists[value] == null) {
-								levelLists[value] = new ArrayList<String>();
+		try {
+			Class.forName("NOSLF4J");
+			System.err.println("SLF4J Disabled.");
+			sDefaultLevel = DISABLED;
+		} catch (ClassNotFoundException classNotFoundException) {
+			// Load and parse the properties.
+			Log.d(SLF4J_TAG, "Trying to load properties from: " + sConfigFile);
+			InputStream in = AndroidLogger.class.getClassLoader()
+					.getResourceAsStream(sConfigFile);
+			if (in == null) {
+				Log.d(SLF4J_TAG, "Trying to load from: " + "META-INF/"
+						+ sConfigFile);
+				in = AndroidLogger.class.getClassLoader().getResourceAsStream(
+						"META-INF/" + sConfigFile);
+			}
+			if (in == null) {
+				Log.d(SLF4J_TAG, "Trying to load from: " + "org/slf4j/"
+						+ sConfigFile);
+				in = AndroidLogger.class.getClassLoader().getResourceAsStream(
+						"org/slf4j/" + sConfigFile);
+			}
+			if (in == null) {
+				Log.d(SLF4J_TAG, "Trying to load from: " + "org/slf4j/impl/"
+						+ sConfigFile);
+				in = AndroidLogger.class.getClassLoader().getResourceAsStream(
+						"org/slf4j/impl/" + sConfigFile);
+			}
+			if (null != in) {
+				Log.d(SLF4J_TAG, "Loading properties...");
+				Properties props = new java.util.Properties();
+				try {
+					props.load(in);
+					for (Enumeration<?> names = props.propertyNames(); names
+							.hasMoreElements();) {
+						String name = (String) names.nextElement();
+						if (name.equals(ANDROID_LEVEL_CHECK)) {
+							ignoreAndroidLevel = !Boolean.parseBoolean(props
+									.getProperty(name));
+						}
+						// What level is this?
+						int value = parseLevel(props.getProperty(name));
+						if (value >= 0) {
+							if (DEFAULT_LEVEL_NAME.equals(name)) {
+								Log.d(SLF4J_TAG, "Setting default level to: "
+										+ levels[value]);
+								sDefaultLevel = value;
+							} else {
+								// Add it to the list for that level.
+								Log.d(SLF4J_TAG, "Setting level for: " + name
+										+ " to: " + levels[value]);
+								if (levelLists[value] == null) {
+									levelLists[value] = new ArrayList<String>();
+								}
+								levelLists[value].add(name);
 							}
-							levelLists[value].add(name);
+						} else {
+							Log.w(SLF4J_TAG, "Unknown level for: " + name
+									+ ": '" + props.getProperty(name)
+									+ "'. Using default.");
 						}
 					}
-					else {
-						Log.w(SLF4J_TAG, "Unknown level for: " + name + ": '" +
-							  props.getProperty(name) +
-							  "'. Using default.");
-					}
+				} catch (IOException e) {
+					Log.e(SLF4J_TAG, "Error while loading properties: "
+							+ e.getMessage());
 				}
-			} catch (IOException e) {
-				Log.e(SLF4J_TAG, "Error while loading properties: " + e.getMessage());
+			} else {
+				Log.w(SLF4J_TAG, "No configuration file found: " + in);
 			}
-		}
-		else {
-			Log.w(SLF4J_TAG, "No configuration file found: " + in);
 		}
 	}
 
-    /**
-     * Package access allows only {@link AndroidLoggerFactory} to instantiate
-     * AndroidLogger instances.
-     */
-    AndroidLogger(String tag) {
+	/**
+	 * Package access allows only {@link AndroidLoggerFactory} to instantiate
+	 * AndroidLogger instances.
+	 */
+	AndroidLogger(String tag) {
 		// Android only supports tags of length <= 23
 		if (tag.length() > 23) {
 			// We try to do something smart here to shorten
@@ -188,15 +207,15 @@ public class AndroidLogger extends MarkerIgnoringBase {
 			String lastPart = parts[parts.length - 1];
 			// Can we use the whole last part?
 			if (lastPart.length() < 23) {
-				if ( ( (parts.length-1) * 2 ) + lastPart.length() <= 23 ) {
-					for (int x=0; x<parts.length - 1; x++) {
+				if (((parts.length - 1) * 2) + lastPart.length() <= 23) {
+					for (int x = 0; x < parts.length - 1; x++) {
 						shortTag.append(parts[x].charAt(0));
 						shortTag.append('.');
 					}
 				}
 				shortTag.append(lastPart);
 			} else {
-				shortTag.append(lastPart.substring(0,10));
+				shortTag.append(lastPart.substring(0, 10));
 				shortTag.append("...");
 				shortTag.append(lastPart.substring(lastPart.length() - 10));
 			}
@@ -211,8 +230,8 @@ public class AndroidLogger extends MarkerIgnoringBase {
 		int foundLevel = -1;
 		for (int i = 0; i < levelLists.length; i++) {
 			if (levelLists[i] != null) {
-				if (levelLists[i].contains(this.tag) ||
-					levelLists[i].contains(tag)) {
+				if (levelLists[i].contains(this.tag)
+						|| levelLists[i].contains(tag)) {
 					foundLevel = i;
 					break;
 				}
@@ -223,152 +242,158 @@ public class AndroidLogger extends MarkerIgnoringBase {
 		} else {
 			this.level = foundLevel;
 		}
-		Log.d(SLF4J_TAG, "Level for: " + this.tag + " set to: " + levels[this.level]);
-    }
+		Log.d(SLF4J_TAG, "Level for: " + this.tag + " set to: "
+				+ levels[this.level]);
+	}
 
-    public void debug(String arg0) {
+	public void debug(String arg0) {
 		if (isDebugEnabled())
 			Log.d(tag, arg0);
-    }
+	}
 
-    public void debug(String arg0, Object arg1) {
+	public void debug(String arg0, Object arg1) {
 		if (isDebugEnabled())
 			Log.d(tag, MessageFormatter.format(arg0, arg1));
-    }
+	}
 
-    public void debug(String arg0, Object[] arg1) {
+	public void debug(String arg0, Object[] arg1) {
 		if (isDebugEnabled())
 			Log.d(tag, MessageFormatter.arrayFormat(arg0, arg1));
-    }
+	}
 
-    public void debug(String arg0, Throwable arg1) {
+	public void debug(String arg0, Throwable arg1) {
 		if (isDebugEnabled())
 			Log.d(tag, arg0, arg1);
-    }
+	}
 
-    public void debug(String arg0, Object arg1, Object arg2) {
+	public void debug(String arg0, Object arg1, Object arg2) {
 		if (isDebugEnabled())
 			Log.d(tag, MessageFormatter.format(arg0, arg1, arg2));
-    }
+	}
 
-    public void error(String arg0) {
+	public void error(String arg0) {
 		if (isErrorEnabled())
 			Log.e(tag, arg0);
-    }
+	}
 
-    public void error(String arg0, Object arg1) {
+	public void error(String arg0, Object arg1) {
 		if (isErrorEnabled())
 			Log.e(tag, MessageFormatter.format(arg0, arg1));
-    }
+	}
 
-    public void error(String arg0, Object[] arg1) {
+	public void error(String arg0, Object[] arg1) {
 		if (isErrorEnabled())
 			Log.e(tag, MessageFormatter.arrayFormat(arg0, arg1));
-    }
+	}
 
-    public void error(String arg0, Throwable arg1) {
+	public void error(String arg0, Throwable arg1) {
 		if (isErrorEnabled())
 			Log.e(tag, arg0, arg1);
-    }
+	}
 
-    public void error(String arg0, Object arg1, Object arg2) {
+	public void error(String arg0, Object arg1, Object arg2) {
 		if (isErrorEnabled())
 			Log.e(tag, MessageFormatter.format(arg0, arg1, arg1));
-    }
+	}
 
-    public void info(String arg0) {
+	public void info(String arg0) {
 		if (isInfoEnabled())
 			Log.i(tag, arg0);
-    }
+	}
 
-    public void info(String arg0, Object arg1) {
+	public void info(String arg0, Object arg1) {
 		if (isInfoEnabled())
 			Log.i(tag, MessageFormatter.format(arg0, arg1));
-    }
+	}
 
-    public void info(String arg0, Object[] arg1) {
+	public void info(String arg0, Object[] arg1) {
 		if (isInfoEnabled())
 			Log.i(tag, MessageFormatter.format(arg0, arg1));
-    }
+	}
 
-    public void info(String arg0, Throwable arg1) {
+	public void info(String arg0, Throwable arg1) {
 		if (isInfoEnabled())
 			Log.i(tag, arg0, arg1);
-    }
+	}
 
-    public void info(String arg0, Object arg1, Object arg2) {
+	public void info(String arg0, Object arg1, Object arg2) {
 		if (isInfoEnabled())
 			Log.i(tag, MessageFormatter.format(arg0, arg1, arg2));
-    }
+	}
 
-    public boolean isDebugEnabled() {
-		return this.level <= DEBUG && (ignoreAndroidLevel || Log.isLoggable(tag, Log.DEBUG));
-    }
+	public boolean isDebugEnabled() {
+		return this.level <= DEBUG
+				&& (ignoreAndroidLevel || Log.isLoggable(tag, Log.DEBUG));
+	}
 
-    public boolean isErrorEnabled() {
-		return this.level <= ERROR && (ignoreAndroidLevel || Log.isLoggable(tag, Log.ERROR));
-    }
+	public boolean isErrorEnabled() {
+		return this.level <= ERROR
+				&& (ignoreAndroidLevel || Log.isLoggable(tag, Log.ERROR));
+	}
 
-    public boolean isInfoEnabled() {
-        return this.level <= INFO && (ignoreAndroidLevel || Log.isLoggable(tag, Log.INFO));
-    }
+	public boolean isInfoEnabled() {
+		return this.level <= INFO
+				&& (ignoreAndroidLevel || Log.isLoggable(tag, Log.INFO));
+	}
 
-    public boolean isTraceEnabled() {
-        return this.level <= TRACE && (ignoreAndroidLevel || Log.isLoggable(tag, Log.VERBOSE));
-    }
+	public boolean isTraceEnabled() {
+		return this.level <= TRACE
+				&& (ignoreAndroidLevel || Log.isLoggable(tag, Log.VERBOSE));
+	}
 
-    public boolean isWarnEnabled() {
-        return this.level <= WARN && (ignoreAndroidLevel || Log.isLoggable(tag, Log.WARN));
-    }
+	public boolean isWarnEnabled() {
+		return this.level <= WARN
+				&& (ignoreAndroidLevel || Log.isLoggable(tag, Log.WARN));
+	}
 
-    public void trace(String arg0) {
+	public void trace(String arg0) {
 		if (isTraceEnabled())
 			Log.v(tag, arg0);
-    }
+	}
 
-    public void trace(String arg0, Object arg1) {
+	public void trace(String arg0, Object arg1) {
 		if (isTraceEnabled())
 			Log.v(tag, MessageFormatter.format(arg0, arg1));
-    }
+	}
 
-    public void trace(String arg0, Object[] arg1) {
+	public void trace(String arg0, Object[] arg1) {
 		if (isTraceEnabled())
 			Log.v(tag, MessageFormatter.arrayFormat(arg0, arg1));
-    }
+	}
 
-    public void trace(String arg0, Throwable arg1) {
+	public void trace(String arg0, Throwable arg1) {
 		if (isTraceEnabled())
 			Log.v(tag, arg0, arg1);
-    }
+	}
 
-    public void trace(String arg0, Object arg1, Object arg2) {
+	public void trace(String arg0, Object arg1, Object arg2) {
 		if (isTraceEnabled())
 			Log.v(tag, MessageFormatter.format(arg0, arg1, arg2));
-    }
+	}
 
-    public void warn(String arg0) {
+	public void warn(String arg0) {
 		if (isWarnEnabled())
 			Log.w(tag, arg0);
-    }
+	}
 
-    public void warn(String arg0, Object arg1) {
+	public void warn(String arg0, Object arg1) {
 		if (isWarnEnabled())
 			Log.w(tag, MessageFormatter.format(arg0, arg1));
-    }
+	}
 
-    public void warn(String arg0, Object[] arg1) {
+	public void warn(String arg0, Object[] arg1) {
 		if (isWarnEnabled())
 			Log.w(tag, MessageFormatter.arrayFormat(arg0, arg1));
-    }
+	}
 
-    public void warn(String arg0, Throwable arg1) {
+	public void warn(String arg0, Throwable arg1) {
 		if (isWarnEnabled())
 			Log.w(tag, arg0, arg1);
-    }
+	}
 
-    public void warn(String arg0, Object arg1, Object arg2) {
+	public void warn(String arg0, Object arg1, Object arg2) {
 		if (isWarnEnabled())
 			Log.w(tag, MessageFormatter.format(arg0, arg1, arg2));
-    }
+	}
 
 }

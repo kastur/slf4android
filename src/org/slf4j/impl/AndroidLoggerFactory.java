2ba597c85e43bca25c46333cf504ee96edf57abf
Nick Palmer <nick@sluggardy.net>
2011-09-19 18:01:38 +0200

    Fixes for the bindings for 1.6.2 which we broke last commit.
    Also added the sources jar just in case you want to step down in.

--- src/org/slf4j/impl/AndroidLoggerFactory.java
+++ src/org/slf4j/impl/AndroidLoggerFactory.java
@@ -7,31 +7,30 @@ import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 
 public class AndroidLoggerFactory implements ILoggerFactory {
-    final static AndroidLoggerFactory SINGLETON = new AndroidLoggerFactory();
+	final static AndroidLoggerFactory SINGLETON = new AndroidLoggerFactory();
 
-    @SuppressWarnings("rawtypes")
+	@SuppressWarnings("rawtypes")
 	Map loggerMap;
 
-    @SuppressWarnings("rawtypes")
-	private AndroidLoggerFactory() {
-        loggerMap = new HashMap();
-    }
+	@SuppressWarnings("rawtypes") AndroidLoggerFactory() {
+		loggerMap = new HashMap();
+	}
 
-    /**
-     * Return an appropriate {@link AndroidLogger} instance by name.
-     */
-    @SuppressWarnings("unchecked")
-    public Logger getLogger(String name) {
-        Logger slogger = null;
-        // protect against concurrent access of the loggerMap
-        synchronized (this) {
-            slogger = (Logger) loggerMap.get(name);
-            if (slogger == null) {
-                slogger = new AndroidLogger(name);
-                loggerMap.put(name, slogger);
-            }
-        }
-        return slogger;
-    }
+	/**
+	 * Return an appropriate {@link AndroidLogger} instance by name.
+	 */
+	@SuppressWarnings("unchecked")
+	public Logger getLogger(String name) {
+		Logger slogger = null;
+		// protect against concurrent access of the loggerMap
+		synchronized (this) {
+			slogger = (Logger) loggerMap.get(name);
+			if (slogger == null) {
+				slogger = new AndroidLogger(name);
+				loggerMap.put(name, slogger);
+			}
+		}
+		return slogger;
+	}
 
 }
